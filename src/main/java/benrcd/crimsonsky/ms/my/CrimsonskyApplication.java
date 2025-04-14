b52f55a3189c4f38e4607cae33ee22dd942e52df
benrcd <richard.benjamin69@yahoo.fr>
2025-04-14 13:56:17 +0200

    kickstart

---
+++ src/main/java/benrcd/crimsonsky/ms/my/CrimsonskyApplication.java
@@ -0,0 +1,13 @@
+package benrcd.crimsonsky.ms.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimsonskyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrimsonskyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/benrcd/crimsonsky/ms/my/CrimsonskyApplicationTests.java
@@ -0,0 +1,13 @@
+package benrcd.crimsonsky.ms.my;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimsonskyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
