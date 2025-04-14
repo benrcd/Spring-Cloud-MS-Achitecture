e7c68154d3d2b69aee1787d3785e5932d0d86490
benrcd <richard.benjamin69@yahoo.fr>
2025-04-14 14:03:19 +0200

    1st ms

---
+++ customer-service/src/test/java/benrcd/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package benrcd.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
