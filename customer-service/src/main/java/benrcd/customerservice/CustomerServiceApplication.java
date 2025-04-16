ae82563283e383352128d0d8348098ad1b664a51
benrcd <richard.benjamin69@yahoo.fr>
2025-04-16 14:40:14 +0200

    Basic first ms done

--- customer-service/src/main/java/benrcd/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/benrcd/customerservice/CustomerServiceApplication.java
@@ -1,7 +1,11 @@
 package benrcd.customerservice;
 
+import benrcd.customerservice.entities.Customer;
+import benrcd.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -10,4 +14,25 @@ public class CustomerServiceApplication {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
+        return args -> {
+            customerRepository.save(Customer.builder()
+                    .name("Ben").email("ben@gmail.com")
+                    .build());
+            customerRepository.save(Customer.builder()
+                    .name("Titi").email("titi@gmail.com")
+                    .build());
+            customerRepository.save(Customer.builder()
+                    .name("Toto").email("toto@gmail.com")
+                    .build());
+            customerRepository.findAll().forEach(c->{
+                System.out.println("=======================");
+                System.out.println(c.getId());
+                System.out.println(c.getName());
+                System.out.println(c.getEmail());
+                System.out.println("=======================");
+            });
+        };
+    }
 }
